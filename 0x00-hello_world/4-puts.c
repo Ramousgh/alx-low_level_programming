7d86ac4a673d230cfd43b8adfdb1e779391585ca
RamouSgh <ramousazawornu@gmail.com>
2022-09-08 18:28:03 -0700

    4-puts.exe

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * this is the entry point
+ * calling the main function
+ * with void
+ */
+
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
+}
+
