720ecadee07af607d5238576c2c298928ba1cfb9
RamouSgh <ramousazawornu@gmail.com>
2022-09-09 17:06:05 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0.
+*/
+
+int main(void)
+{
+
+char cab;
+
+for (cab = 'a'; cab <= 'z'; cab++)
+
+putchar(cab);
+
+putchar('\n');
+return (0);
+}
