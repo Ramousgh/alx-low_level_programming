8006a8f5d951cafec1b7c86416902a615e885ebf
RamouSgh <ramousazawornu@gmail.com>
2022-09-09 17:29:50 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0.
+ */
+
+int main(void)
+{
+
+char zyx;
+
+for (zyx = 'z'; zyx >= 'a'; zyx--)
+
+putchar(zyx);
+
+putchar('\n');
+return (0);
+
+}
