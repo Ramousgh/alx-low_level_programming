3a02a4877e3f6279bf54a7d57f2b087fd6828630
RamouSgh <ramousazawornu@gmail.com>
2022-09-13 16:57:04 -0700

    o-_putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* main - Entry
+* Description: Write a program that prints _putchar, followed by a new line
+* Return: Always 0 (Success)
+*/
+int _putchar(char);
+int main(void)
+{
+int _putchar('_');
+int _putchar('p');
+int _putchar('u');
+int _putchar('t');
+int _putchar('c');
+int _putchar('h');
+int _putchar('a');
+int _putchar('r');
+int _putchar('\n');
+return (0);
+}
