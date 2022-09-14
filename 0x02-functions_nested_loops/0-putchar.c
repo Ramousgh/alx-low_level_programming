246f076cb485630c0627142ab36ac2d8e35a6b80
RamouSgh <ramousazawornu@gmail.com>
2022-09-13 17:24:19 -0700

    "0-putchar.c"

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,16 @@
 * Description: Write a program that prints _putchar, followed by a new line
 * Return: Always 0 (Success)
 */
-int _putchar(char);
 int main(void)
 {
-int _putchar(char '_');
-int _putchar(char 'p');
-int _putchar(char 'u');
-int _putchar(char 't');
-int _putchar(char 'c');
-int _putchar(char 'h');
-int _putchar(char 'a');
-int _putchar(char 'r');
-int _putchar(char '\n');
+int _putchar('_');
+int _putchar('p');
+int _putchar('u');
+int _putchar('t');
+int _putchar('c');
+int _putchar('h');
+int _putchar('a');
+int _putchar('r');
+int _putchar('\n');
 return (0);
 }
