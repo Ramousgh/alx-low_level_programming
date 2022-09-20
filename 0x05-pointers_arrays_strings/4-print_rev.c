0519a7289f19fdba7217cd432b5dbd3ebd193cc8
RamouSgh <ramousazawornu@gmail.com>
2022-09-19 17:08:22 -0700

    "4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints strings in reverse
+ * @s: input
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	for (i = 0; s[i] != '\0', i++)
+
+	for (i = i + 1; i >= 0; c--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
